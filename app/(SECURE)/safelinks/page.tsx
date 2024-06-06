219cd30f3bed5a7a1aa1693c1379ba8c783e2508
vilmao0 <koller.vilmos.sandor@outlook.com>
2024-06-06 18:28:25 +0200

    xyív

---
+++ app/(SECURE)/safelinks/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SafeLinks = () => {
+  return (
+    <div>
+        Create your own SafeLinks NOW!
+        <div>------------------</div>
+        <a href="/payments/pay/safelinks/5pcs"><button type="submit" className='bg-teal-700 rounded-[32] flex-col'>CREATE YOUR FISRT SAFELINK!</button></a>
+    </div>
+    
+  )
+}
+
+export default SafeLinks
---
+++ app/(root)/payments/pay/safelinks/5pcs/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TESTX2 = () => {
+  return (
+    <div>buy.stripe.com/bsoj5/monthly</div>
+  )
+}
+
+export default TESTX2
