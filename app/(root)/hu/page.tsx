f0fb4aab027a58b7a54b39f434ab9d91f02f058a
vilmao0 <koller.vilmos.sandor@outlook.com>
2024-06-06 18:16:39 +0200

    styling  Hu

--- app/(root)/hu/page.tsx
+++ app/(root)/hu/page.tsx
@@ -8,10 +8,19 @@ export const metadata = {
 
 const Hu = () => {
   return (
-    <div className='bg-slate-900 text-3xl'>
-      <h1 className='font-sans font-semibold text-emerald-200'>
+    <div className='bg-slate-900 text- justify-between pb-[100%]'>
+      <h1 className='font-mono font-semibold text-emerald-200'>
         Üdv!
       </h1>
+      <h1 className='font-mono text-4xl text-emerald-300'>
+        A nevem Vilmi(Vilmos) 
+
+
+        
+      </h1>
+      
+      
+      <p className=''>copyright @ Vilmi</p>
       </div>
       
 
