1ea36e3712a6e3490186fb91557a558924eb5551
vilmao0 <168273738+vilmao0@users.noreply.github.com>
2024-06-09 16:41:43 +0200

    jjj

--- app/(root)/hu/page.tsx
+++ app/(root)/hu/page.tsx
@@ -13,7 +13,8 @@ const Hu = () => {
         Üdv!
       </h1>
       <h1 className='font-mono text-4xl text-emerald-300'>
-        Geri butus!
+        Vilmihu link shortener
+        
 
 
         
